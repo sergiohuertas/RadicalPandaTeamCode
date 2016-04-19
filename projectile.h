f8620286b993f372bdff8a4f5254234772003efc
Eduardo Ibáñez Frutos <edu.music1994@gmail.com>
2016-04-19 13:39:50 +0200

    Update projectile.h

--- projectile.h
+++ projectile.h
@@ -2,22 +2,28 @@
 #ifndef PROJECTILE_H
 #define PROJECTILE_H
 #include <SFML/Graphics.hpp>
-
 class projectile {
 public:
-    projectile();
+    projectile(float newmspeed, int newdirection, bool newfriendly, int newdamage, float newangle, float x_player, float y_player);
+    //projectile(const projectile& orig);
     void move();
+    virtual ~projectile();
     void setDirection(int d);
     void setPosition(int x, int y);
+    void setAngle(int a);
     int getXPosition();
     int getYPosition();
     void draw(sf::RenderWindow &window);
 private:
-    int mSpeed=2;
-    int direction=0; // 1 - up, 2 - down, 3 - left, 4 - right
-    bool friendly=true; // False - Enemigo
-    int damage=5;
-    sf::RectangleShape rect; // Sprite
+    float mSpeed;
+    int direction; // 1 - up, 2 - down, 3 - left, 4 - right
+    bool friendly; // False - Enemigo
+    int damage;
+    float angle;
+    sf::Sprite *sprite;
+    sf::Texture *tx_flecha;
+    
+    //sf::RectangleShape rect; // Sprite
 };
 
 #endif
